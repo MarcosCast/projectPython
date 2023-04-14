d45778bbbb07b5a40516c93fd34c7bdeb0845b0f
MarcosCast <marcosmax.5@gmail.com>
2023-04-14 18:31:31 -0300

    Criando o camingo loogin

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadastro")
+    path('cadastro/', views.cadastro, name="cadastro"),
+    path('login/', views.login, name="login")
     
 ]
--- users/views.py
+++ users/views.py
@@ -54,14 +54,20 @@ def cadastro(request):
         # TODO: Validar força da senha, retornar depois para ajustar
 
         if not (senha == confirmar_senha):    
+            messages.add_message(request, constants.ERROR, 'As senhas estão diferentes!')
             return redirect(reverse('cadastro'))
         
         user = User.objects.filter(username=username)
 
         if user.exists():
-            return redirect('/users/cadastro')   
+            messages.add_message(request, constants.ERROR, 'Esse usuário já existe!')
+            return redirect(reverse('cadastro'))  
         
         user = User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso!')
         #user.save()
-        # return redirect(reverse('login'))
-        return HttpResponse('Teste')
+        return redirect(reverse('login'))
+    
+def login(request):
+  if request.method == "GET":
+    return render(request, 'login.html')
