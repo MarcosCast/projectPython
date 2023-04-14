df97af2d5e8e7ea8f75f709ae8b784d99453d391
MarcosCast <marcosmax.5@gmail.com>
2023-04-14 19:22:52 -0300

    Criando tabela para o cadastro dos eventos

--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+@login_required
 
 # Create your views here.
+def novo_evento(request):
+    if request.method == "GET":
+        return render(request, 'novo_evento.html')
--- first_project/urls.py
+++ first_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('eventos/', include('eventos.urls')),
     #path('usuarios/', include('usuarios.urls')),
 ]
