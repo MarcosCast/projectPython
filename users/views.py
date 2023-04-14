c73bbe093bb8fbeb1bbf102ee2a2c8c65183535b
MarcosCast <marcosmax.5@gmail.com>
2023-04-14 18:59:06 -0300

    Finalizando login

--- users/views.py
+++ users/views.py
@@ -1,39 +1,3 @@
-'''from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def cadastro(request):
-    if request.method == "GET":
-       return render(request, 'cadastro.html')
-    elif request.method == "POST":
-        username = request.POST.get(from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.contrib.auth.models import User
-
-def cadastro(request):
-    if request.method == "GET":
-        return render(request, 'cadastro.html')
-    elif request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        confirmar_senha = request.POST.get('confirmar_senha')
-
-        # TODO: Validar força da senha, retornar depois para ajustar
-
-        if not (senha == confirmar_senha):    
-            return redirect(reverse('cadastro'))
-        
-        user = User.objects.filter(username=username)
-
-        if user.exists():
-            return redirect('/users/cadastro')   
-        
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        #user.save()
-        # return redirect(reverse('login'))
-        return HttpResponse('Teste'))'''
-
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -41,6 +5,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -71,3 +36,15 @@ def cadastro(request):
 def login(request):
   if request.method == "GET":
     return render(request, 'login.html')
+  elif request.method == "POST":
+    username = request.POST.get('username')
+    senha = request.POST.get('senha')
+
+    user = auth.authenticate(username=username, password=senha)
+
+    if not user:
+        messages.add_message(request, constants.ERROR, 'Username ou senha inválidos!')
+        return redirect(reverse('login'))
+        
+    auth.login(request, user)
+    return redirect('/eventos/novo_evento/')
