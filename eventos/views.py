8e4c563beeacd605042a724bf1c936589ed50ab1
MarcosCast <marcosmax.5@gmail.com>
2023-04-15 19:09:32 -0300

    Criando função exportar csv

--- eventos/views.py
+++ eventos/views.py
@@ -6,6 +6,10 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.http import Http404
+import csv 
+from secrets import token_urlsafe
+import os
+from django.conf import settings
 
 @login_required
 
@@ -74,3 +78,20 @@ def participantes_evento(request, id):
     if request.method == "GET":
         participantes = evento.participantes.all()#[::3]
         return render(request, 'participantes_evento.html', {'evento': evento, 'participantes': participantes})
+    
+def gerar_csv(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    participantes = evento.participantes.all()
+    
+    token = f'{token_urlsafe(6)}.csv'
+    path = os.path.join(settings.MEDIA_ROOT, token)
+
+    with open(path, 'w') as arq:
+        writer = csv.writer(arq, delimiter=",")
+        for participante in participantes:
+            x = (participante.username, participante.email)
+            writer.writerow(x)
+
+    return redirect(f'/media/{token}')
