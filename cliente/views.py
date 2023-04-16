f4b743c60d68d87b5b884bf808e0c4750cca0859
MarcosCast <marcosmax.5@gmail.com>
2023-04-16 19:19:09 -0300

    Criando page de meus certificados

--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from eventos.models import Certificado
 
 # Create your views here.
+def meus_certificados(request):
+    certificados = Certificado.objects.filter(participante=request.user)
+    return render(request, 'meus_certificados.html', {'certificados': certificados})
